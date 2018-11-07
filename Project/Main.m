b36ec4407875b2f8400bf79291f2b0b5c30f393d
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2018-11-07 11:37:48 +0530

    Now it optimizes image by removing huge objects

--- Project/Main.m
+++ Project/Main.m
@@ -28,18 +28,20 @@ figure();
 imshow(image);
 title('Image after converting into Black and white:');
 
-% Remove all object containing fewer than 20 pixels
+% Remove all object containing fewer than 15 pixels
 figure();
 imshow(image);
 image = bwareaopen(image,15);
 figure();
 imshow(image);
-title('After removing all objects fewer tha 20 pixels:');
+title('After removing all objects fewer tha 15 pixels:');
+
+% Removes all objects contaiing larger than 70000 pixels
+image = image - bwareaopen(image,70000);
+figure();
+imshow(image);
+title('After removing all objects fewer tha 15 pixels:');
 
 %Storage matrix word from image
 word=[ ];
 re=image;
-image = image - bwareaopen(image,70000);
-figure();
-imshow(image);
-title('Did something');
